c43c062447269f1e238482c7a3b4f1d810097ffb
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-05-08 17:47:41 +0200

    commit

---
+++ ej_JSiwng_Primitiva/src/ej_jsiwng_primitiva/Ej_JSiwng_Primitiva.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ej_jsiwng_primitiva;
+
+
+public class Ej_JSiwng_Primitiva {
+
+
+    public static void main(String[] args){
+      
+    }
+}
